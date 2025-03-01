accb293a19255209eb2c80f108b0c2f95b76fe26
RaffaelHub <raffiscoffe@gmail.com>
2025-03-01 16:43:43 +0000

    SUPER_TRUNFO_3.0

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-// Não utilizei fgets, por pedido do professor, já que é para ser uma estrutura simples.
+// Não usei o fgets, por pedido do professor, já que é para ser uma estrutura simples.
+// Grande gama de comentarios, para o melhor entendimento.
 
 
 
@@ -29,19 +30,19 @@ int main() {
         // Solicitando os dados do cadastro ao usuario, PRIMEIRA CARTA
     
     printf("Digite uma letra de 'A' a 'H': ");
-    scanf(" %c", &estado1);
+    scanf(" %c", &estado1); // Armazena o estado da carta de 'A' a 'H'
     printf("Digite o código da carta (exemplo: A01): ");
     scanf("%s", codigo_carta1); // não usei o &(pointer) nas variaveis que tem array, segunda minhas pesquisas, não precisa.
     printf("Digite o nome da cidade (Sem espaços): ");
-    scanf("%s", cidade1);
+    scanf("%s", cidade1); // Armazena o nome da cidade(sem espaços)
     printf("Digite a quantidade de habitantes da cidade: ");
-    scanf("%d", &populacao1);
+    scanf("%d", &populacao1); // Armazena os habitantes(população)
     printf("Digite A área da cidade (em quilômetros quadrados): ");
-    scanf("%f", &area1);
+    scanf("%f", &area1); // Armazena a área quadrada da cidade
     printf("Digite o PIB(Produto Interno Bruto) da cidade: ");
-    scanf("%f", &pib1);
+    scanf("%f", &pib1); // Armazena o PIB da cidade
     printf("Digite o número de Pontos Turísticos da cidade: ");
-    scanf("%d", &ponto_turistico1); 
+    scanf("%d", &ponto_turistico1); // Armazena a quantidade de pontos turisticos
    
 
 
@@ -60,19 +61,19 @@ int main() {
          // Cadastro da SEGUNDA CARTA
     
     printf("Digite uma letra de 'A' a 'H': ");
-    scanf(" %c", &estado2);
+    scanf(" %c", &estado2); // Armazena o estado da carta de 'A' a 'H'
     printf("Digite o código da carta (exemplo: A01): ");
-    scanf("%s", codigo_carta2);
+    scanf("%s", codigo_carta2); // Armazenar o código da carta
     printf("Digite o nome da cidade (Sem espaços): ");
-    scanf("%s", cidade2);
+    scanf("%s", cidade2); // Armazena o nome da cidade(sem espaços)
     printf("Digite a quantidade de habitantes da cidade: ");
-    scanf("%d", &populacao2);
+    scanf("%d", &populacao2); // Armazena os habitantes(população)
     printf("Digite a área da cidade (em quilômetros quadrados): ");
-    scanf("%f", &area2);
+    scanf("%f", &area2);  // Armazena a área quadrada da cidade
     printf("Digite o PIB(Produto Interno Bruto) da cidade: ");
-    scanf("%f", &pib2);
+    scanf("%f", &pib2); // Armazena o PIB da cidade
     printf("Digite o número de pontos turisticos da cidade: ");
-    scanf("%d", &ponto_turistico2);
+    scanf("%d", &ponto_turistico2); // Armazena a quantidade de pontos turisticos
   
 
 
@@ -117,6 +118,8 @@ int main() {
 
     
     // fim do programa
+
+    // Feito com carinho (:
 
     return 0;
 }
